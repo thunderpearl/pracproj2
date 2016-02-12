a322cad9daf4bb32e961ff810eeed424f14dca96
lightningbolt275@gmail.com <lightningbolt275@gmail.com>
2016-02-12 17:13:41 +0530

    This is the php file for front page of website

---
+++ woww.php
@@ -0,0 +1,168 @@
+<! DOCTYPE html>
+<html>
+    <head>
+        <link type = "text/css" rel = "stylesheet" href = "http:\\localhost:8080\\woww.css"/>
+        <title> wow 3.3.5 WOTLK </title>
+        
+        <style>
+            body
+            {
+                
+                background-attachment: fixed;
+            }
+        </style>
+    </head>
+
+
+    
+    <body background = "http:\\localhost:8080\\1634231.jpg">
+
+        <nav id="nav01"></nav>  <! This "Nav" element is very important for creating the top bar :D :D :D >
+        <footer id="foot01"></footer>
+    
+        
+
+        
+        <div id="choice">
+            <h2 style="font-style:roman; color:#A385C2; text-align: center">REALM</h2>
+            <div class = "content"><a href="https://www.warmane.com/"> Deathwing </a></div>
+            <div class = "content"><a href="https://www.warmane.com/"> Lordaeron </a></div>
+            <div class = "content"><a href="https://www.warmane.com/"> Ragnaros </a></div>
+            <div class = "content"><a href="https://www.warmane.com/"> Frostwolf </a></div>
+        </div>
+
+
+
+            
+        <div id = "mid">
+
+
+            <div id="mid1">
+                    <h3 class="team">Allaince</h3>
+                    <p>Warmane is andasdasda da, da,da, a, c,a c,a csa c,a c,a c, ac,ac,a c,as c, a,c s,mcsa ,mc ,ma c,msa c,mas c,sa ca,m c,msa c,mas csa,m </p>
+                    <pre></pre  >
+
+                    <audio controls>  <!--This code is for putting the audio file in the webpage :D :D :D -->
+                        <source src="Blank Space.ogg" type="audio/ogg">
+                        <source src="Blank Space - Taylor Swift.mp3" type="audio/mpeg">
+                    <!Your browser does not support the audio element.>
+                    </audio>
+
+
+                        <pre></pre>
+
+                    <video width="360" controls>
+                        <source src="World of Warcraft - Trailer.mp4" type="video/mp4">
+                        <source src="mov_bbb.ogg" type="video/ogg">
+                        Your browser does not support HTML5 video.
+                    </video>
+            </div>
+
+
+
+
+                
+            <div id="mid2">
+                    <h3 class="team">Horde</h3>
+                    <p>lets see what it is going to be dnas,dnsa s a, a , a,s  a xa,nd,sana ,s a as x,  a,ma,. xa, sa sa,fsfna fas, asasndlas  c,a clascnlkn,a  dandlka ld al dl dla </p>
+
+                    <pre></pre>
+                    <pre></pre>
+
+                    <audio controls>  <!--This code is for putting the audio file in the webpage :D :D :D -->
+                        <source src="Blank Space.ogg" type="audio/ogg">
+                        <source src="Wildest Dreams - Taylor Swift.mp3" type="audio/mpeg">
+                     <!Your browser does not support the audio element.>
+                    </audio>
+
+                    <pre></pre>
+                    <!iframe width="360">
+                       <! src="https://www.youtube.com/watch?v=Wq4Y7ztznKc"> 
+                        <! // This code is for getting video directly from Youtube but not working.>
+                    <!/iframe>
+
+            </div>
+                
+            
+
+
+            <div id="currentnews">
+                    
+                <h2 id="newstitle"> Realm News </h2>
+                    <p class="data">fdsgfdsfsfsfdsfdffffsffdfdfdsfdfgsdggsgsdgdsgdfdsfdsfsdfsdfdf</p>
+                    <p class="data">fdsgfdsfsfsfdsfdffffsffdfdfdsfdfgsdggsgsdgdsgdfdsfdsfsdfsdfdf</p>
+                    <p class="data">fdsgfdsfsfsfdsfdffffsffdfdfdsfdfgsdggsgsdgdsgdfdsfdsfsdfsdfdf</p>
+                    <p class="data">fdsgfdsfsfsfdsfdffffsffdfdfdsfdfgsdggsgsdgdsgdfdsfdsfsdfsdfdf</p>
+                    <p class="data">fdsgfdsfsfsfdsfdffffsffdfdfdsfdfgsdggsgsdgdsgdfdsfdsfsdfsdfdf</p>
+                    <p class="data">fdsgfdsfsfsfdsfdffffsffdfdfdsfdfgsdggsgsdgdsgdfdsfdsfsdfsdfdf</p>
+                    <p class="data">fdsgfdsfsfsfdsfdffffsffdfdfdsfdfgsdggsgsdgdsgdfdsfdsfsdfsdfdf</p>
+                   
+                    
+            </div>    
+                
+        </div>
+
+
+
+      
+        <div id="userdata">
+           
+            <pre></pre>
+               
+               <form name="login">
+                   <label for="userid">Username</label>
+                   <input type="text" name="userid" id="userid"/>
+                   <pre></pre>    
+
+                   <label for="pswrd">Password</label>
+                   <input type="password" name="pswrd" id="pswrd"/> 
+            
+                <!--// This is the  normal username and password form. 
+                // "type" refers to the what type of data is gonna be.
+        
+                // "name's" value "userid" will refers to how we will be handling the input in the "name" field.
+                // Like in below javascript how we gonna handle the value of name.
+                // Value of name will be  handled by "userid"
+                -->
+          
+                   <pre></pre>
+                   <input type="button" onclick="check(this.form)" value="Login"/>
+                   <input type="reset" value="Cancel"/>
+                   <a href="wowwreg5.php"><input type="button" value="New User"></a>   <! This way we forwarded a page to  other  page on click.>    
+                    
+                   <br></br>
+                    <input type="search" value="" name="search" >
+                </form>
+
+           
+                
+
+                <script language="javascript">
+                  
+                    function check(form) { /*function to check userid & password*/
+                    /*the following code checkes whether the entered userid and password are matching*/
+                    if(form.userid.value == "myuserid" && form.pswrd.value == "mypswrd") {
+                    window.open('Supporting_page.html')/*opens the target page while Id & password matches*/
+                    }
+
+                    else {
+                        alert("Error Password or Username")/*displays error message*/
+                         }
+
+                    <p>fgjgjg</p>
+                
+                    }
+               
+
+                </script>
+          
+        </div>
+       
+
+                 <script src="woww.js"></script>
+   
+
+    </body>
+
+
+</html>
